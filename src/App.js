6d56926e58c3ec83f9533d05cbbb386fe26a3acc
Jean <zikvanz@gmail.com>
2023-07-19 08:39:52 -0500

    feat(Header): maquetar botones

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      
       <Footer></Footer>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,31 @@
 function Header() {
-    return <p>Header</p>;
-  }
-  
-  export default Header;
-  
+  return (
+    <div className="flex justify-between py-2 px-4">
+      <div>
+        <p className="text-xl font-black">humankind</p>
+      </div>
+      <div className="flex gap-1">
+        <button className="py-2 px-4 font-bold hover:text-gray-500 transition">
+          Art
+        </button>
+        <button className="py-2 px-4 font-bold hover:text-gray-500 transition">
+          Collectibles
+        </button>
+        <button className="py-2 px-4 font-bold hover:text-gray-500 transition">
+          Studios
+        </button>
+        <button className="py-2 px-4 font-bold hover:text-gray-500 transition">
+          Community
+        </button>
+        <button className="py-2 px-4 font-bold hover:text-gray-500 transition">
+          Subscribe
+        </button>
+        <button className="bg-black text-white py-2 px-7 rounded-full font-bold border-4 border-black hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition">
+          Play now! O
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
