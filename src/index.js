5b7f770580cc387694a7870bedf6c68af6412ad9
Jean <zikvanz@gmail.com>
2023-07-20 20:37:15 -0500

    feat: add sections

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/scss/styles.scss'
+import './assets/styles/scss/styles.scss';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// import './assets/fonts/Lora/Lora-VariableFont_wght.ttf';
-// import './assets/fonts/Lora/Lora-VariableFont_wght.ttf';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,5 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import homeTopImage from './../../assets/images/62d1b8e2c048c969e47514c7_hero-top-p-1600.webp';
 import homeBottomImage from './../../assets/images/62d1b8e32e16f6b524514c60_hero-bottom-p-1600.webp';
 import playImage from './../../assets/images/646fb24a3ad2f2ac41afd67b_herokind-adventure-lores-p-800.png';
+import bringImage from './../../assets/images/63e09ea82ae9b06d12ea9b91_028-band-playing-p-800.webp';
+import grabImage from './../../assets/images/63765b76de5f1356947d9d23_Lunchboxes.gif';
 
 function Home() {
   return (
@@ -24,7 +26,7 @@ function Home() {
         </p>
       </div>
       <div className="w-2/3 m-auto mt-28">
-        <div className="flex gap-3 border-4 border-black rounded-3xl p-6 transition hover:shadow-lg">
+        <div className="flex gap-4 border-4 card-shadow border-black rounded-3xl p-6 transition ">
           <div className="w-1/2">
             <img src={playImage} className="rounded-lg" alt="playImage" />
           </div>
@@ -44,6 +46,95 @@ function Home() {
               <button className="mt-8 text-xl bg-yellow-300 text-black py-4 px-6 rounded-full font-bold border-4  border-yellow-300 hover:border-lime-300 hover:bg-lime-300 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition">
                 ⚔️ Play Now!
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-2/3 m-auto mt-16">
+        <div className="flex gap-4 border-4 card-shadow border-black rounded-3xl p-6 transition ">
+          <div className="w-1/2">
+            <img src={bringImage} className="rounded-lg" alt="bringImage" />
+          </div>
+          <div className="w-1/2 text-left flex  items-center">
+            <div>
+              <p className="text-4xl font-bold">
+                Bring your ideas to life with the Humankind Fund
+              </p>
+              <p className="text-xl mt-6 font-lora">
+                The Humankind Fund is a community-driven platform that supports
+                and empowers projects that align with the values and mission of
+                the Humankind brand. Join us in bringing creative ideas to life
+                and making a positive impact in the world.
+              </p>
+              <button className="mt-8 text-xl bg-yellow-300 text-black py-4 px-6 rounded-full font-bold border-4  border-yellow-300 hover:border-lime-300 hover:bg-lime-300 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition">
+                ❤️ Learn More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-2/3 m-auto mt-16">
+        <div className="flex gap-4 border-4 card-shadow border-black rounded-3xl p-6 transition ">
+          <div className="w-1/2">
+            <img src={grabImage} className="rounded-lg" alt="grabImage" />
+          </div>
+          <div className="w-1/2 text-left flex  items-center">
+            <div>
+              <p className="text-4xl font-bold">
+                Grab your lunchbox. It will be a long journey.
+              </p>
+              <p className="text-xl mt-6 font-lora">
+                We are delighted you’re finally joining our adventure alongside
+                Maya and Dallas. It all starts with a simple and nostalgic
+                object: a lunchbox. A lunchbox? What for? Well, it’s gonna be a
+                long journey!
+                <br />
+                <br />
+                Every lunchbox will unlock a Humankind on a brave quest. You’ll
+                be able to claim your Kind with them. We’ve made our generative
+                lunchboxes with art we’ve been creating for the last year.
+              </p>
+              <div className="flex items-center gap-1 mt-8">
+                <button className="bg-black text-xl text-white py-4 px-6 rounded-full font-bold border-4 border-black hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition">
+                  🌊 Get one from Opensea
+                </button>
+                <button className="py-4 px-6 text-xl rounded-full font-bold border-4 border-black transition hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5">
+                  🌮 Learn more
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-2/3 m-auto mt-16">
+        <div className="flex gap-4 border-4 card-shadow border-black rounded-3xl p-6 transition ">
+          <div className="w-1/2">
+            <img src={grabImage} className="rounded-lg" alt="grabImage" />
+          </div>
+          <div className="w-1/2 text-left flex  items-center">
+            <div>
+              <p className="text-4xl font-bold">
+                Grab your lunchbox. It will be a long journey.
+              </p>
+              <p className="text-xl mt-6 font-lora">
+                We are delighted you’re finally joining our adventure alongside
+                Maya and Dallas. It all starts with a simple and nostalgic
+                object: a lunchbox. A lunchbox? What for? Well, it’s gonna be a
+                long journey!
+                <br />
+                <br />
+                Every lunchbox will unlock a Humankind on a brave quest. You’ll
+                be able to claim your Kind with them. We’ve made our generative
+                lunchboxes with art we’ve been creating for the last year.
+              </p>
+              <div className="flex items-center gap-1 mt-8">
+                <button className="bg-black text-xl text-white py-4 px-6 rounded-full font-bold border-4 border-black hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition">
+                  🌊 Get one from Opensea
+                </button>
+                <button className="py-4 px-6 text-xl rounded-full font-bold border-4 border-black transition hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5">
+                  🌮 Learn more
+                </button>
+              </div>
             </div>
           </div>
         </div>
