d484fcb939a5fd0c914267b59dd0da568ac93aa3
Jean <zikvanz@gmail.com>
2024-01-05 22:12:44 -0500

    feat(Wrestle): add responsive to subfooter

--- src/pages/Wrestle/Wrestle.js
+++ src/pages/Wrestle/Wrestle.js
@@ -94,11 +94,18 @@ export default function Wrestle() {
         </div>
       </div>
       <div className="flex justify-center py-28">
-        <div className="w-4/5 grid grid-cols-7 gap-5" >
-          <div className="col-span-3 text-left flex items-center">
+        <div className="w-4/5 grid grid-cols-7 gap-5 md:gap-8">
+          <div className="col-span-7 md:col-span-4 md:order-last">
+            <img src={fireImg} alt="fireImg" className="w-full max-h-full" />
+          </div>
+          <div className="col-span-7 md:col-span-3 text-left flex items-center justify-center">
             <div>
-              <p className="font-bold text-6xl mt-10">Humankind</p>
-              <p className="font-lora text-xl ">stay human. be kind.</p>
+              <p className="font-bold text-4xl mt-10 text-center md:text-left">
+                Humankind
+              </p>
+              <p className="font-lora text-xl text-center md:text-left">
+                stay human. be kind.
+              </p>
               <div className="mt-8 flex gap-6">
                 <img src={discordImg} alt="discordImg" className="w-8" />
                 <img src={twiterImg} alt="twiterImg" className="w-8" />
@@ -106,21 +113,18 @@ export default function Wrestle() {
                 <img src={linkedinImg} alt="linkedinImg" className="w-8" />
                 <img src={openSeaImg} alt="openSeaImg" className="w-8" />
               </div>
-              <div>
-                <button className="mt-6 text-lg py-2 px-5 rounded-full font-bold border-4 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition bg-black text-white border-black hover:bg-red-500">
+              <div className="flex flex-col mt-8 gap-3 lg:flex-row">
+                <button className="py-2 px-5 rounded-full font-bold border-4 transition bg-black text-white border-black hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 lg:text-lg">
                   ⚔️ Play Now!
                 </button>
-                <button className="text-black border-black ml-2 mt-6 text-lg py-2 px-5 rounded-full font-bold border-4 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 transition">
+                <button className="text-black border-black py-2 px-5 rounded-full font-bold border-4 transition hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 lg:text-lg">
                   👾 Join our Discord
                 </button>
               </div>
-              <p className="mt-10 font-lora text-sm">
+              <p className="mt-10 font-lora text-sm text-center lg:text-left">
                 Humankind Industries Inc. 2023
               </p>
             </div>
-          </div>
-          <div className="col-span-4">
-            <img src={fireImg} alt="fireImg" className="w-full max-h-full" />
           </div>
         </div>
       </div>
