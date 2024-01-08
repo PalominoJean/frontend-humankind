0de875d8b440c64d67f22ff68e5fe0f3e34c113b
Jean <zikvanz@gmail.com>
2024-01-08 17:40:51 -0500

    feat(Collectibles): add section subfooter

--- src/pages/Collectibles/Collectibles.js
+++ src/pages/Collectibles/Collectibles.js
@@ -11,9 +11,13 @@ import ipfsImg from "./../../assets/images/icons-ipfs.svg";
 import utilityImg from "./../../assets/images/icons-utility.svg";
 import voteImg from "./../../assets/images/icons-vote.svg";
 import mapImg from "./../../assets/images/icons-world_map.svg";
-import studiosImg from "./../../assets/images/Studios.webp";
-import artFundImg from "./../../assets/images/Fund.webp";
 import mushroomImg from "./../../assets/images/mushroom-sticker.png";
+import discordImg from "./../../assets/images/discord.svg";
+import twiterImg from "./../../assets/images/twitter_squared.svg";
+import instagramImg from "./../../assets/images/icons8-instagram.svg";
+import linkedinImg from "./../../assets/images/icons8-linkedin.svg";
+import openSeaImg from "./../../assets/images/OpenSea.svg";
+import fireImg from "./../../assets/images/fire-p-500.webp";
 
 export default function Collectibles() {
   return (
@@ -184,13 +188,50 @@ export default function Collectibles() {
               alt="mushroomImg"
               className="w-32 mix-blend-multiply"
             />
-            <p className="font-bold text-4xl mt-2 lg:text-5xl lg:mt-2">Let's hangout!</p>
+            <p className="font-bold text-4xl mt-2 lg:text-5xl lg:mt-2">
+              Let's hangout!
+            </p>
             <p className="font-lora mt-1 text-lg lg:mt-4">
               Stop by and say hello; we're looking to make some new friends.
             </p>
             <button className="py-3 px-6 rounded-full font-bold border-4 transition bg-black text-white border-black mt-7 hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 md:w-auto lg:text-lg lg:mt-8 ">
               Join our Discord
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center py-28">
+        <div className="w-4/5 grid grid-cols-7 gap-5 md:gap-8">
+          <div className="col-span-7 md:col-span-4 md:order-last">
+            <img src={fireImg} alt="fireImg" className="w-full max-h-full" />
+          </div>
+          <div className="col-span-7 md:col-span-3 text-left flex items-center justify-center">
+            <div>
+              <p className="font-bold text-5xl mt-10 text-center md:text-left md:text-6xl xl:text-7xl">
+                Humankind
+              </p>
+              <p className="font-lora text-lg text-center md:text-left md:text-xl xl:text-2xl">
+                stay human. be kind.
+              </p>
+              <div className="mt-8 flex gap-6">
+                <img src={discordImg} alt="discordImg" className="w-8" />
+                <img src={twiterImg} alt="twiterImg" className="w-8" />
+                <img src={instagramImg} alt="instagramImg" className="w-8" />
+                <img src={linkedinImg} alt="linkedinImg" className="w-8" />
+                <img src={openSeaImg} alt="openSeaImg" className="w-8" />
+              </div>
+              <div className="flex flex-col mt-8 gap-3 lg:flex-row">
+                <button className="py-2 px-5 rounded-full font-bold border-4 transition bg-black text-white border-black hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 lg:text-lg xl:py-3 xl:px-7">
+                  ⚔️ Play Now!
+                </button>
+                <button className="text-black border-black py-2 px-5 rounded-full font-bold border-4 transition hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 lg:text-lg">
+                  👾 Join our Discord
+                </button>
+              </div>
+              <p className="mt-10 font-lora text-sm text-center lg:text-left">
+                Humankind Industries Inc. 2023
+              </p>
+            </div>
           </div>
         </div>
       </div>
