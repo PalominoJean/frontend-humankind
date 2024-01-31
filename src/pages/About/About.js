63f63d7e2b82a65fe17623bc30e0d9d794343ede
Jean <zikvanz@gmail.com>
2024-01-31 18:36:52 -0500

    feat(About): add section "Pablo Stanley"

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,7 +8,8 @@ import squad12Img from "./../../assets/images/squad12.png";
 import squad163Img from "./../../assets/images/squad163.png";
 import squad195Img from "./../../assets/images/squad195.png";
 import squad46Img from "./../../assets/images/squad46.png";
-
+import pabsImg from "./../../assets/images/pabs-humankind-bw 1-p-500.webp";
+import { useState } from "react";
 
 export default function About() {
   const squad = [
@@ -18,6 +19,13 @@ export default function About() {
     { image: squad195Img, name: "Adam", work: "Artist" },
     { image: squad46Img, name: "Maya Draisin", work: "Brand Advisor" },
   ];
+
+  const [showResume, setShowResume] = useState(false);
+
+  const handleSetShowMenu = () => {
+    setShowResume(!showResume);
+  };
+
   return (
     <>
       <div className="w-11/12 m-auto grid grid-cols-11 gap-5 py-24 lg:w-10/12 xl:w-8/12 xl:py-40">
@@ -70,20 +78,70 @@ export default function About() {
         </p>
       </div>
       <div className="w-6/12 m-auto py-24 md:w-8/12 lg:w-10/12 xl:w-9/12 2xl:w-7/12 xl:py-40">
-        <p className="text-center font-bold text-3xl md:text-4xl lg:text-5xl xl:text-6xl">The Squad</p>
-        <div className="grid grid-cols-12 gap-y-5 mt-5 justify-center xl:grid-cols-10">
+        <p className="text-center font-bold text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+          The Squad
+        </p>
+        <div className="flex flex-wrap gap-y-5 mt-5 justify-center ">
           {squad.map(({ image, name, work }) => {
             return (
               <div
                 key={name}
-                className="col-span-12 text-center sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-2 "
+                className="w-full text-center sm:w-6/12 md:w-4/12 lg:w-3/12 2xl:w-2/12"
               >
                 <img src={image} alt="squad62Img" className=" w-full" />
-                <p className="font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">{name}</p>
-                <p className="font-lora md:text-lg xl:text-xl 2xl:text-2xl">{work}</p>
+                <p className="font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+                  {name}
+                </p>
+                <p className="font-lora md:text-lg xl:text-xl 2xl:text-2xl">
+                  {work}
+                </p>
               </div>
             );
           })}
+        </div>
+      </div>
+      <div className="flex justify-center p-5 sm:p-10 md:p-14">
+        <div className="bg-black text-white pb-10 px-5 rounded-3xl text-center flex items-center flex-col max-w-3xl sm:px-10 md:px-16">
+          <img
+            src={pabsImg}
+            alt="pabsImg"
+            className="w-40 rounded-full border-4 border-black -mt-10 md:-mt-16 lg:w-52"
+          />
+          <p className="text-3xl font-semibold mt-5 sm:text-4xl lg:text-5xl 2xl:text-6xl">
+            Who is Pablo Stanley?
+          </p>
+          <p className="mt-3 font-lora lg:text-lg 2xl:text-xl">
+            Pablo Stanley is a passion-driven artist, co-founder, and CEO of
+            Bueno, creator of iconic NFT collections, and one of the most
+            influential forces in the illustration world. He's behind
+            open-source illustrations used by millions of people worldwide like
+            Humaaans, Open Peeps, Avataaars, Open Doodles, Buttts, and Bottts.
+            <br /> <br />
+            <span className={`${showResume ? "block" : "hidden"}`}>
+              His life purpose is to fill the world with doodles. This dream has
+              led him to develop two ambitious projects: a children's animated
+              show with TIME Studios and Bueno, a no-code platform for NFT
+              artists. <br /> <br /> Through his evolution as a designer,
+              entrepreneur, and artist, Pablo has found ways to empower other
+              creators by sharing his process with tutorials on his youtube
+              channel, Sketch Together, and by giving talks, workshops, and
+              conferences worldwide. In his free time, he sketches silly comics.
+              He has been inspiring a whole generation of creators through these
+              mediums of expression. <br /> <br /> Pablo believes that art and
+              storytelling are the mightiest weapons. With Humankind, he strives
+              to support artistic and passionate souls to live from what they do
+              best: creating and sharing their talent with humanity.
+            </span>
+          </p>
+          <button
+            className="mt-9 text-xl font-lora text-gray-500"
+            onClick={() => handleSetShowMenu()}
+          >
+            Read {showResume ? "Less ↑" : "More ↓"}
+          </button>
+          <button className="mt-10 py-3 px-6 rounded-full font-bold border-4 transition bg-black text-white hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 md:w-auto lg:text-lg lg:mt-8 ">
+            Say hi on Twitter
+          </button>
         </div>
       </div>
     </>
