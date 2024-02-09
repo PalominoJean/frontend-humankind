725d8ef610bc5c4072954b142486e9d523982025
Jean <zikvanz@gmail.com>
2024-02-09 10:47:43 -0500

    feat(Collectibles): add animations

--- src/pages/collectibles/Collectibles.js
+++ src/pages/collectibles/Collectibles.js
@@ -1,9 +1,13 @@
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 //images
 import runLeftImg from "./../../assets/images/run-run-left.png";
 import runRightImg from "./../../assets/images/run-run-right.png";
 import stickersImg from "./../../assets/images/stickers-website.webp";
 import lunchboxesImg from "./../../assets/images/Lunchboxes.gif";
-import treeImg from "./../../assets/images/Tree.webp";
+import treeImg from "./../../assets/images/tree.webp";
+import treeBackgroundImg from "./../../assets/images/tree-background.webp";
 import lunchboxImg from "./../../assets/images/Lunchbox.webp";
 import nftImg from "./../../assets/images/hk-8.webp";
 import membershipImg from "./../../assets/images/icons-membership.svg";
@@ -18,6 +22,16 @@ import SubFooter from "../../components/Subfooter";
 import Button from "../../components/Button";
 import SectionChildren from "../components/SectionChildren";
 
+const variants = {
+  visible: { scale: 1, transition: { duration: 0.5 } },
+  hidden: { scale: 0.9 },
+};
+
+const variants2 = {
+  visible: { scale: 1, transition: { duration: 0.5 } },
+  hidden: { scale: 1.1 },
+};
+
 const sectionChildrenData = {
   title: "stayhuman",
   cards: [
@@ -30,107 +44,196 @@ const sectionChildrenData = {
 };
 
 export default function Collectibles() {
+  const controls1 = useAnimation();
+  const controls2 = useAnimation();
+  const controls3 = useAnimation();
+  const controls4 = useAnimation();
+  const controls5 = useAnimation();
+  const controls6 = useAnimation();
+  const controls7 = useAnimation();
+  const controls8 = useAnimation();
+
+  const [ref1, inView1] = useInView();
+  const [ref2, inView2] = useInView();
+  const [ref3, inView3] = useInView();
+  const [ref4, inView4] = useInView();
+  const [ref5, inView5] = useInView();
+  const [ref6, inView6] = useInView();
+  const [ref7, inView7] = useInView();
+  const [ref8, inView8] = useInView();
+
+  useEffect(() => {
+    controls1.start(inView1 ? "visible" : "hidden");
+    controls2.start(inView2 ? "visible" : "hidden");
+    controls3.start(inView3 ? "visible" : "hidden");
+    controls4.start(inView4 ? "visible" : "hidden");
+    controls5.start(inView5 ? "visible" : "hidden");
+    controls6.start(inView6 ? "visible" : "hidden");
+    controls7.start(inView7 ? "visible" : "hidden");
+    controls8.start(inView8 ? "visible" : "hidden");
+  }, [
+    controls1,
+    controls2,
+    controls3,
+    controls4,
+    controls5,
+    controls6,
+    controls7,
+    controls8,
+    inView1,
+    inView2,
+    inView3,
+    inView4,
+    inView5,
+    inView6,
+    inView7,
+    inView8,
+  ]);
+
   return (
     <>
-      <div className="w-11/12 m-auto grid grid-cols-11 gap-5 mt-24 lg:w-10/12 xl:w-8/12">
-        <div className="col-span-11 md:col-span-5 flex flex-col justify-center text-left items-start">
-          <p className="font-semibold uppercase text-xl 2xl:text-2xl">
-            HUMANKIND collectibles
-          </p>
-          <h1 className="text-5xl font-bold mt-2 md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
-            Time for an adventure.
-          </h1>
-          <p className="mt-4 font-lora text-lg md:text-xl lg:text-2xl">
+      <div className="w-11/12 m-auto grid grid-cols-11 gap-5 py-28 lg:w-10/12 xl:w-9/12">
+        <div className="col-span-11 flex flex-col gap-10 justify-center text-left items-start md:col-span-5">
+          <div className="flex flex-col gap-1">
+            <p className="font-semibold uppercase text-xl 2xl:text-2xl">
+              HUMANKIND collectibles
+            </p>
+            <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+              Time for an adventure.
+            </h1>
+          </div>
+          <p className="font-lora text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
             After a long time of doodling we're launching our first collection.
           </p>
-          <br />
           <Button text="Mint Now" type="secondary"></Button>
         </div>
         <div className="col-span-11 md:col-span-6 relative">
-          <img
+          <motion.img
+            ref={ref1}
+            animate={controls1}
+            initial="hidden"
+            variants={variants}
             src={runLeftImg}
             alt="runLeftImg"
             className="w-full max-h-full absolute"
           />
-          <img
+          <motion.img
+            ref={ref2}
+            animate={controls2}
+            initial="hidden"
+            variants={variants2}
             src={runRightImg}
             alt="runRightImg"
             className="w-full max-h-full"
           />
         </div>
       </div>
-      <div className="mt-40">
-        <img
+      <div>
+        <motion.img
+          ref={ref3}
+          animate={controls3}
+          initial="hidden"
+          variants={variants}
           src={stickersImg}
           alt="stickersImg"
-          className="h-20 md:h-24 lg:h-32 xl:h-44"
+          className="h-20 object-cover md:h-24 lg:h-32 xl:h-44"
         />
       </div>
-      <div className="w-11/12 m-auto grid grid-cols-11 gap-5 mt-28 lg:w-9/12 xl:w-7/12  xl:gap-8 2xl:w-6/12">
+      <div className="w-11/12 m-auto grid grid-cols-11 gap-5 py-28 lg:w-9/12 xl:w-7/12  xl:gap-8 2xl:w-6/12">
         <div className="col-span-11">
-          <img
+          <motion.img
+            ref={ref4}
+            animate={controls4}
+            initial="hidden"
+            variants={variants}
             src={lunchboxesImg}
             alt="lunchboxesImg"
             className="w-10/12 m-auto max-h-full xl:w-8/12"
           />
         </div>
         <div className="col-span-11 md:col-span-5">
-          <p className="text-3xl font-bold md:text-4xl lg:text-5xl">
+          <p className="text-3xl font-semibold sm:text-4xl md:text-5xl 2xl:text-6xl">
             Every adventure starts with a healthy meal 🖤.
           </p>
         </div>
         <div className="col-span-11 md:col-span-6">
-          <p className="font-lora text-lg md:text-xl lg:text-2xl xl:leading-10 2xl:text-3xl 2xl:leading-10">
+          <p className="font-lora text-lg sm:text-xl md:text-2xl lg:text-2xl xl:leading-10 2xl:text-3xl">
             First, you need to get a lunchbox that will serve to wake up your
             Humankind. You can only start the adventure as soon as your Kind is
             well-nourished!
           </p>
         </div>
       </div>
-      <div className="w-11/12 m-auto grid grid-cols-11 gap-5 mt-28 lg:w-9/12 xl:w-7/12 xl:gap-8 xl:mt-40 2xl:w-6/12">
-        <div className="col-span-11 md:col-span-5 flex flex-col justify-center items-start">
-          <img
+      <div className="w-11/12 m-auto grid grid-cols-11 gap-5 py-28 lg:py-36 lg:w-9/12 xl:w-7/12 xl:gap-8 2xl:py-40 2xl:w-6/12">
+        <div className="col-span-11 flex flex-col justify-center items-start gap-8  md:col-span-5 2xl:gap-10">
+          <motion.img
+            ref={ref5}
+            animate={controls5}
+            initial="hidden"
+            variants={variants}
             src={lunchboxImg}
             alt="lunchboxImg"
-            className="w-20 lg:w-32 max-h-full"
+            className="w-20 max-h-full md:w-28 lg:w-32 2xl:w-40"
           />
-          <p className="text-3xl font-bold mt-6 md:text-4xl lg:text-5xl ">
+          <p className="text-3xl font-semibold sm:text-4xl md:text-5xl 2xl:text-6xl">
             Yum! Feed your kind!
           </p>
-          <p className="font-lora text-lg mt-4 md:text-xl lg:text-2xl xl:leading-10">
+          <p className="font-lora text-lg sm:text-xl md:text-2xl lg:text-2xl xl:leading-10 2xl:text-3xl">
             Since your Kind will take the lunchbox with them, you will have to
             do an exchange. Soon we will announce more details of this.
           </p>
-          <br />
           <Button text="Mint Now" type="secondary"></Button>
         </div>
-        <div className="col-span-11 md:col-span-6">
-          <img src={treeImg} alt="treeImg" className="w-full max-h-full" />
+        <div className="col-span-11 flex items-center relative h-96 md:h-auto md:col-span-6">
+          <motion.img
+            ref={ref6}
+            animate={controls6}
+            initial="hidden"
+            variants={variants}
+            src={treeBackgroundImg}
+            alt="treeBackgroundImg"
+            className="w-full absolute"
+          />
+          <motion.img
+            ref={ref7}
+            animate={controls7}
+            initial="hidden"
+            variants={variants2}
+            src={treeImg}
+            alt="treeImg"
+            className="w-full absolute"
+          />
         </div>
       </div>
       <div className="pb-24 pt-48 section-collectibles">
-        <div className="w-11/12 m-auto md:w-10/12 lg:w-9/12 xl:w-7/12">
+        <div className="w-11/12 m-auto md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12">
           <div className="border-black rounded-3xl transition bg-black text-white">
             <div className="w-11/12 m-auto text-center pb-10 md:w-10/12 lg:w-9/12 xl:pb-16">
-              <div className="flex items-center m-auto flex-wrap justify-center w-11/12 pl-7 md:w-11/12 xl:w-10/12">
+              <motion.div
+                ref={ref8}
+                animate={controls8}
+                initial="hidden"
+                variants={variants}
+                className="flex items-center m-auto flex-wrap justify-center w-11/12 pl-7 md:w-11/12 xl:w-10/12"
+              >
                 {[...Array(9)].map((item, index) => (
                   <img
                     src={nftImg}
                     alt="nftImg"
-                    className="rounded-full border-4 border-black w-24 -mt-7 -ml-7 hover:scale-110 transition relative sm:w-28  md:w-32 lg:w-36 xl:w-48"
+                    className="rounded-full border-4 border-black w-24 -mt-7 -ml-7 hover:scale-110 transition relative sm:w-28 md:w-32 lg:w-36 2xl:w-48"
                     key={index}
                   />
                 ))}
-              </div>
-              <p className="text-3xl font-bold mt-5 md:text-4xl lg:text-5xl xl:text-6xl">
+              </motion.div>
+              <p className="text-3xl font-semibold mt-5 md:text-4xl lg:text-5xl 2xl:text-6xl">
                 Humankind Collectibles
               </p>
-              <p className="mt-6 font-lora lg:text-lg xl:text-2xl">
+              <p className="mt-6 font-lora lg:text-lg xl:text-xl 2xl:text-2xl">
                 We're launching a set of diverse and unique art collectibles
                 featuring the Humankind species, the most prevalent in The Land
                 of Kinds.
-              </p>
-              <p className="text mt-6 font-lora lg:text-lg xl:text-2xl">
+                <br />
+                <br />
                 Through ownership, these art pieces will unlock exclusive access
                 to the Humankind world and support the LINK . Collectors will
                 participate in the ongoing creation of the world, get sneak
@@ -145,7 +248,9 @@ export default function Collectibles() {
                     alt="membershipImg"
                     className="w-8 xl:w-10"
                   />
-                  <p className="font-lora xl:text-xl">Community Pass</p>
+                  <p className="font-lora xl:text-lg 2xl:text-xl">
+                    Community Pass
+                  </p>
                 </div>
                 <div className="flex flex-col items-center gap-2">
                   <img
@@ -153,11 +258,15 @@ export default function Collectibles() {
                     alt="diamondImg"
                     className="w-8 xl:w-10"
                   />
-                  <p className="font-lora xl:text-xl">Minted in ETH</p>
+                  <p className="font-lora xl:text-lg 2xl:text-xl">
+                    Minted in ETH
+                  </p>
                 </div>
                 <div className="flex flex-col items-center gap-2">
                   <img src={ipfsImg} alt="ipfsImg" className="w-8 xl:w-10" />
-                  <p className="font-lora xl:text-xl">IPFS Hosted</p>
+                  <p className="font-lora xl:text-lg 2xl:text-xl">
+                    IPFS Hosted
+                  </p>
                 </div>
                 <div className="flex flex-col items-center gap-2">
                   <img
@@ -165,50 +274,35 @@ export default function Collectibles() {
                     alt="utilityImg"
                     className="w-8 xl:w-10"
                   />
-                  <p className="font-lora xl:text-xl">Benefits Unlock</p>
+                  <p className="font-lora xl:text-lg 2xl:text-xl">
+                    Benefits Unlock
+                  </p>
                 </div>
                 <div className="flex flex-col items-center gap-2">
                   <img src={voteImg} alt="voteImg" className="w-8 xl:w-10" />
-                  <p className="font-lora xl:text-xl">Community-Driven</p>
+                  <p className="font-lora xl:text-lg 2xl:text-xl">
+                    Community-Driven
+                  </p>
                 </div>
                 <div className="flex flex-col items-center gap-2">
                   <img src={mapImg} alt="mapImg" className="w-8 xl:w-10" />
-                  <p className="font-lora xl:text-xl">Staking Mechanism</p>
+                  <p className="font-lora xl:text-lg 2xl:text-xl">
+                    Staking Mechanism
+                  </p>
                 </div>
               </div>
-              <div className="mt-8 xl:mt-12">
+              <div className="flex flex-col justify-center items-center mt-8 xl:mt-12">
                 <Button text="Mint Now" type="secondaryDark"></Button>
-                <p className="mt-5 font-lora">View on Etherscan</p>
+                <button className="mt-5 font-lora block border-b-2 text-lg">
+                  View on Etherscan
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="bg-black">
-        <p className="text-white font-bold font-size-21vw line-height-20rem">
-          stayhuman
-        </p>
-        <div className="flex justify-center items-center text-left gap-10 mt-16 section-stayhuman p-5">
-          <div className="bg-white px-6 py-8 rounded-xl border-4 flex flex-col items-center border-black duration-500 text-center hover:bg-yellow-400 md:px-10 md:py-12">
-            <img
-              src={mushroomImg}
-              alt="mushroomImg"
-              className="w-32 mix-blend-multiply"
-            />
-            <p className="font-bold text-4xl mt-2 lg:text-5xl lg:mt-2 2xl:text-6xl">
-              Let's hangout!
-            </p>
-            <p className="font-lora mt-1 text-lg lg:mt-4 2xl:text-xl">
-              Stop by and say hello; we're looking to make some new friends.
-            </p>
-            <button className="py-3 px-6 rounded-full font-bold border-4 transition bg-black text-white border-black mt-7 hover:bg-red-500 hover:shadow-md hover:translate-x-0.5 hover:-translate-y-0.5 md:w-auto lg:text-lg lg:mt-8 ">
-              Join our Discord
-            </button>
-          </div>
-        </div>
-      </div> */}
-      <SectionChildren data={sectionChildrenData}></SectionChildren>
-      <SubFooter></SubFooter>
+      <SectionChildren data={sectionChildrenData} />
+      <SubFooter />
     </>
   );
 }
--- src/pages/components/SectionChildren.js
+++ src/pages/components/SectionChildren.js
@@ -17,7 +17,7 @@ export default function SectionChildren({ data }) {
 
   return (
     <>
-      <div className="bg-black h-120vh flex justify-center items-center mt-16 relative overflow-hidden">
+      <div className="bg-black h-120vh flex justify-center items-center relative overflow-hidden">
         <div className="flex flex-col gap-5 max-w-screen-lg z-10 px-3 sm:flex-row sm:justify-center 2xl:max-w-screen-xl">
           {data.cards.map(({ image, title, text }, index) => (
             <div
